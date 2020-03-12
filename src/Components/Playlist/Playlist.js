d663ae24cce14c05031220570e13882dcf34a859
jgartsu12 <jgartsu12@gmail.com>
2020-03-12 13:47:00 -0400

    passed in state to search results

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './Playlist.css';
-import TrackList from '../TrackList/TrackList';
+// import TrackList from '../TrackList/TrackList';
 
 class Playlist extends React.Component {
     render() {
